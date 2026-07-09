d2c68b7032f2ea91c5eef55916e2ca93e21b8a20
Dylan Bolin <dylanbolin09@gmail.com>
2026-07-09 23:02:15 +0800

    refactor: 简化测试代码中的小部件包装

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,9 +6,7 @@ import 'package:liquid_glass_widgets/liquid_glass_widgets.dart';
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(
-      LiquidGlassWidgets.wrap(
-        child: const ProviderScope(child: MyApp()),
-      ),
+      LiquidGlassWidgets.wrap(child: const ProviderScope(child: MyApp())),
     );
 
     // 初始路由是 /inbox，应显示 InboxPage 中的提示文字
